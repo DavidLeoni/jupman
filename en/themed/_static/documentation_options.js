040de3e986664067a3231416a0e6c82a747903ed
GitHub Action <action@github.com>
2022-04-18 06:35:11 +0000

    Update documentation

--- en/themed/_static/documentation_options.js
+++ en/themed/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.3.0-75-g480da41',
+    VERSION: '3.3.0-76-g4d6f373',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
