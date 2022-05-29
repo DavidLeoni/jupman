2cc8c9dfc36bfa5da6c83aa8ff13fad008bdc154
David Leoni <noreply@davidleoni.it>
2022-05-29 07:09:01 +0200

    Github Actions automated test attempt 10 reverted fail test on purpose #99

--- _test/jupman_tools_test.py
+++ _test/jupman_tools_test.py
@@ -22,9 +22,6 @@ import nbsphinx
 from common_test import * 
 import datetime
 
-def test_fail_on_purpose():
-    assert True == False
-
 def test_detect_release():
     res =  jmt.detect_release()
     assert res == 'dev' or len(res.split('.')) >= 2
